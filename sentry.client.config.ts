7967c9cc251cc4f3493230c2b820fde4fdc71465
RUI Song <rxs456@cs.bham.ac.uk>
2024-07-19 23:50:14 +0100

    edits

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://e300b41df2ffb6d4592972400a25c0d3@o4506876178464768.ingest.us.sentry.io/4507159179034624",
+  dsn: "https://2e37b76280e74511a740f7348c41b5fd@o4507630442774528.ingest.de.sentry.io/4507630446444624",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
